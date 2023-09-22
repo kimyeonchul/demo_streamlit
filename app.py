eb09b0720ebefa651a29f04406f61652c5dab2e2
kimyeonchul <yeonchul0807@nate.com>
2023-09-22 18:35:27 +0900

    1

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import openai
 import streamlit as st
 
-api_key = st.text_input('openai api key')
+# OpenAI API 키 입력
+api_key = st.text_input('OpenAI API Key')
 if api_key:
     openai.api_key = api_key
 
+# 번역할 언어 목록
 lang_list = ('영어', '중국어', '일본어', '아랍어', '한국어')
 
 st.header('번역기')
@@ -13,23 +15,34 @@ col1, col2 = st.columns(2)
 result = ''
 with col1:
     option = st.selectbox('Lang', lang_list)
-    q = st.text_area('From')
+    q = st.text_area('번역할 텍스트 입력')
     if q:
+        # 번역을 위한 프롬프트 생성
+        if option == '영어':
+            target_lang = 'en'
+        elif option == '중국어':
+            target_lang = 'zh'
+        elif option == '일본어':
+            target_lang = 'ja'
+        elif option == '아랍어':
+            target_lang = 'ar'
+        elif option == '한국어':
+            target_lang = 'ko'
+        
+        prompt = f"번역: {target_lang} >> {q}"  # 프롬프트 생성
 
-				# :+:+:+:+: prompt format 을 만들어보세요 :+:+:+:+:
-				# option = 영어, 중국어.. 중 하나 
-				# q = 입력 문구
-        prompt = q # <- 코드를 수정하세요
-
+        # GPT-3 API를 사용하여 번역 요청
         response = openai.Completion.create(
-            model='text-davinci-003',
+            engine="text-davinci-003",
             prompt=prompt,
-            temperature=0,
             max_tokens=100,
-            top_p=1,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
+            stop=None,
         )
-        print(response.choices[0].text.strip())
+
         result = response.choices[0].text.strip()
 
 with col2:
-    st.text_area('To', value=result)
+    st.text_area('번역 결과', value=result)
